ea2b3634f341ed39b47441ec290f8815b12e8c39
KALORSOR <alorsorgodsway@gmail.com>
2024-05-21 03:03:33 -0700

    Improve Engines

--- models/state.py
+++ models/state.py
@@ -25,3 +25,12 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_l.append(city)
             return city_l
+    if not isinstance(storage, DBStorage):
+        @property
+        def cities(self):
+            """Returns the list of City objects from storage linked to the current State."""
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
